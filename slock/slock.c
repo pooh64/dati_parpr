04a1e0a75a9eb8f0a6d19d8febb1e5c62ad9956e
Vsevolod Pukhov <pukhov.vyu@phystech.edu>
2020-03-20 02:12:53 +0300

    Remove inline from libslock

--- slock/slock.c
+++ slock/slock.c
@@ -3,22 +3,22 @@
 
 size_t const slock_t_size = sizeof(pthread_mutex_t);
 
-inline int slock_init(slock_t *sl)
+int slock_init(slock_t *sl)
 {
 	return pthread_mutex_init((void*) sl, NULL);
 }
 
-inline int slock_destroy(slock_t *sl)
+int slock_destroy(slock_t *sl)
 {
 	return pthread_mutex_destroy((void*) sl);
 }
 
-inline int slock_lock(slock_t *sl)
+int slock_lock(slock_t *sl)
 {
 	return pthread_mutex_lock((void*) sl);
 }
 
-inline int slock_unlock(slock_t *sl)
+int slock_unlock(slock_t *sl)
 {
 	return pthread_mutex_unlock((void*) sl);
 }
